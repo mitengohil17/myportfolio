56f62041a4bd20ad7dbf7ad120a600bce88c675c
Miten Gohil <93910975+mitengohil17@users.noreply.github.com>
2022-06-27 21:34:50 +0100

    Add files via upload

---
+++ Java Projects/Banking Application/First_Bank.java
@@ -0,0 +1,17 @@
+// Visit compscicentral.com, the #1 resource for Comp Sci students
+
+public class First_Bank {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account Miten = new Account("Miten Gohil", "A00001");
+
+
+        Account Sunshine = new Account("Sunshine", "B00002");
+        Sunshine.showMenu();
+
+
+    }
+
+}
